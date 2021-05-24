7c7d41c9190091a56313eeae5f2bfac4e4502bca
Edward <jaysongraphics@gmail.com>
2021-05-24 17:23:32 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+let submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    })
+     .then(Response => Response.json())
+     .then((newID) => {
+         document.body.innerHTML = newID['id']
+        })
+        
+        .catch((error) => {
+          document.body.innerHTML = error.message
+    })
+}
